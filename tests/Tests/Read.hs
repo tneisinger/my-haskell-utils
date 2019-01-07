c80691223656e637ecfd641c9ed391b746d7702e
tjneisi <tjneisi@gmail.com>
2019-01-06 21:46:22 -0800

    Add quickCheck tests of maybeRead

--- tests/Tests/Read.hs
+++ tests/Tests/Read.hs
@@ -1,15 +1,40 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Tests.Read
   ( testReadModule
   ) where
 
 import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.StringRandom (matchRegexp)
+import Data.Text (unpack)
+import Data.Char (isDigit)
 
 import MyUtils.Read
 import MyUtils.Console
 
+perhapsReadableAsInt :: Gen String
+perhapsReadableAsInt = unpack <$> matchRegexp "(\\d|[a]){1,2}"
+
+prop_maybeRead_canReadInts :: Property
+prop_maybeRead_canReadInts =
+  forAll perhapsReadableAsInt preprop_maybeRead_canReadAllDigits
+
+preprop_maybeRead_canReadAllDigits :: String -> Bool
+preprop_maybeRead_canReadAllDigits str =
+  if all isDigit str
+     then maybeRead str == Just (read str :: Int)
+     else maybeRead str == (Nothing :: Maybe Int)
+
+-- This line is necessary in order for the forAllProperties function to work
+-- Something to do with template haskell
+return []
+
 testReadModule :: IO ()
 testReadModule = do
   colorPutStrLn Red "\nTesting the Read module..."
+  _ <- $quickCheckAll
   hspec $ do
     describe "maybeRead" $ do
       it "returns (Just 31) when given '31' and typed as Integer" $ do
