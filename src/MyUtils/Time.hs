32637f76dc6d474c6a721762ee4454c1cdc6732d
tjneisi <tjneisi@gmail.com>
2019-01-07 16:00:00 -0800

    Add documentation to MyUtils.Time

--- src/MyUtils/Time.hs
+++ src/MyUtils/Time.hs
@@ -6,14 +6,26 @@ import Data.Time.Calendar (toGregorian)
 
 import MyUtils.Show (showIntegralWZeros)
 
+{-|
+  This type is used to specify the format of a date string.
+-}
 data DateFormat = YYYYMMDD | YYYYDDMM | YYMMDD | YYDDMM
   deriving (Eq, Show)
 
+
+{-|
+  Given a UTCTime, return the (year, month, day)
+-}
 getDateFromUTCTime :: Integral a => UTCTime -> (a, a, a)
 getDateFromUTCTime t =
   let (y, m ,d) = toGregorian . utctDay $ t
    in (fromIntegral y, fromIntegral m, fromIntegral d)
 
+
+{-|
+  Given a DateFormat, a separator string, and a UTCTime, return
+  a string that represents the date of the given UTCTime value.
+-}
 makeStrFromUTCTime :: DateFormat -> String -> UTCTime -> String
 makeStrFromUTCTime fmt sep t =
   let dateInts :: (Int, Int, Int)
@@ -27,6 +39,11 @@ makeStrFromUTCTime fmt sep t =
         YYMMDD   -> intercalate sep [drop 2 yStr, mStr, dStr]
         YYDDMM   -> intercalate sep [drop 2 yStr, dStr, mStr]
 
+
+{-|
+  Given a DateFormat and a separator string, return today's date
+  in the requested format.
+-}
 getTodayStr :: DateFormat -> String -> IO String
 getTodayStr fmt sep =
   getCurrentTime >>= return . makeStrFromUTCTime fmt sep
